54d57275f0e718ad9b4558c2b4a46f8fe8ce676d
zqy <850074816@qq.com>
2023-07-21 18:32:44 +0000

    add TryVariables

--- procedure_oriented/procedureOriented/src/main/java/com/deltavoid/procedureOriented/App.java
+++ procedure_oriented/procedureOriented/src/main/java/com/deltavoid/procedureOriented/App.java
@@ -1,5 +1,9 @@
 package com.deltavoid.procedureOriented;
 
+
+// import com.deltavoid.procedureOriented.*;
+
+
 /**
  * Hello world!
  *
@@ -9,5 +13,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        TryVariables.main();
     }
 }
---
+++ procedure_oriented/procedureOriented/src/main/java/com/deltavoid/procedureOriented/TryVariables.java
@@ -0,0 +1,43 @@
+package com.deltavoid.procedureOriented;
+
+
+
+
+public class TryVariables {
+    
+	public static void main(){
+		
+		byte a1=1, b1=2;
+		int  s1= a1 + b1 ;
+		System.out.println(s1);
+		
+		short a2=1, b2=2;
+		int  s2= (short)a2 + b2 ;
+		System.out.println(s2);
+		
+		int a3=1, b3=2;
+		int s3= a3 + b3 ;
+		System.out.println(s3);
+		
+		long a4=1, b4=2;
+		long s4= a4 + b4;
+		System.out.println(s4);
+		
+		char a5='a', b5='b';
+		//char s5= a5 + b5 ;
+		
+		boolean a6=true, b6=false;
+		boolean s6= a6 && b6 ;
+		System.out.println(s6);
+		
+		float a7=0.1f , b7=0.2f;
+		float s7= a7 + b7 ;
+		System.out.println(s7);
+		
+		double a8=0.1, b8=0.2;
+		double s8= a8 + b8 ;
+		System.out.println(s8);
+		
+	}
+
+}
