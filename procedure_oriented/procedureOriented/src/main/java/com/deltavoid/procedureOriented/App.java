07b1c9de231ee837ab312d78bc069e174feeb201
zqy <850074816@qq.com>
2023-07-22 16:40:07 +0000

    add  ArithmeticDemo

--- procedure_oriented/procedureOriented/src/main/java/com/deltavoid/procedureOriented/App.java
+++ procedure_oriented/procedureOriented/src/main/java/com/deltavoid/procedureOriented/App.java
@@ -28,6 +28,8 @@ public class App {
 
         PrintExample.printZ();
 
+        ArithmeticDemo.main();
+
     }
 
 }
---
+++ procedure_oriented/procedureOriented/src/main/java/com/deltavoid/procedureOriented/ArithmeticDemo.java
@@ -0,0 +1,25 @@
+package com.deltavoid.procedureOriented;
+
+public class ArithmeticDemo {
+
+	public static void main() {
+		// TODO Auto-generated method stub
+		int result = 1 + 2; // result is now 3
+        System.out.println(result);
+
+        result -= 1; // result is now 2
+        System.out.println(result);
+
+        result *= 2; // result is now 4
+        System.out.println(result);
+
+        result /= 2; // result is now 2
+        System.out.println(result);
+
+        result += 8; // result is now 10
+        result %= 7; // result is now 3
+        System.out.println(result);
+
+	}
+
+}
