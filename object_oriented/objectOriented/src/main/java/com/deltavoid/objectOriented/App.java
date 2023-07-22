43d8396ce5cdc4b331f9c98fb779f267e61a180a
zqy <850074816@qq.com>
2023-07-22 16:15:26 +0000

    add objectOriented

---
+++ object_oriented/objectOriented/src/main/java/com/deltavoid/objectOriented/App.java
@@ -0,0 +1,13 @@
+package com.deltavoid.objectOriented;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
